a8e5a430382721a155d9fc07a1691a60f2369e64
TAPAEB <aleksejpushkin223@gmail.com>
2022-08-16 13:33:36 +0300

    Сделал логику корзину и юзера

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(cors())
 app.use(express.json());
 app.use(require("./routes/user.route.js"));
 app.use(require("./routes/product.route.js"));
+app.use(require("./routes/cart.route.js"))
 
 mongoose
   .connect(process.env.MONGO_SERVER,
