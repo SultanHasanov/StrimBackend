f6f4a3d413d9ed9db80fac52e5d2e85bb0a8a841
TAPAEB <aleksejpushkin223@gmail.com>
2022-08-17 13:57:10 +0300

    Настройка логики

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -1,8 +1,22 @@
 const { set } = require('mongoose')
 const Cart = require('../models/Cart.model.js')
 const Product = require('../models/Product.model.js')
+const User = require('../models/User.model.js')
 
 module.exports.cartController = {
+    getCart: async (req, res) => {
+        try {
+            const { userId } = req.params
+            const setUser = await User.findById(userId)
+            const setCart = await Cart.find({
+                userId: setUser._id
+            })
+            const setCartId = String(setCart[0]._id)
+            res.json(await Cart.findById(setCartId))
+        } catch (e) {
+            res.json(e)
+        }
+    }, 
     postCart: async (req, res) => {
         try {
             const { userId } = req.params
@@ -16,7 +30,13 @@ module.exports.cartController = {
     },
     productAddCart: async (req, res) => {
         try {
-            const { cartId } = req.params
+            const { userId } = req.params
+            const setUser = await User.findById(userId)
+            const setCart = await Cart.find({
+                userId: setUser._id
+            })
+            const setCartId = String(setCart[0]._id)
+
             const { product } = req.body
 
             const setProduct = await Product.findById(product)
@@ -25,7 +45,7 @@ module.exports.cartController = {
                 return res.json("Нет на складе")
             }
 
-            await Cart.findByIdAndUpdate(cartId, {
+            await Cart.findByIdAndUpdate(setCartId, {
                 $push: {
                     products: {
                         productId: product
@@ -45,20 +65,26 @@ module.exports.cartController = {
     },
     productDeleteCart: async (req, res) => {
         try {
-            const { cartId } = req.params
+            const { userId } = req.params
+            const setUser = await User.findById(userId)
+            const setCart = await Cart.find({
+                userId: setUser._id
+            })
+            const setCartId = String(setCart[0]._id)
+
             const { product } = req.body
             
             const setProduct = await Product.findById(product)
-            const setCart = await Cart.findById(cartId)
-            const setDeleteProduct = setCart.products.filter((el) => String(el.productId) === String(setProduct._id))
-            const setProducts = setCart.products.filter((el) => String(el.productId) !== String(setProduct._id))
+            const setCart0 = await Cart.findById(setCartId)
+            const setDeleteProduct = setCart0.products.filter((el) => String(el.productId) === String(setProduct._id))
+            const setProducts = setCart0.products.filter((el) => String(el.productId) !== String(setProduct._id))
             
             await Product.findByIdAndUpdate(product, {
                 
                 left: setProduct.left + setDeleteProduct[0].amount
             })
 
-            await Cart.findByIdAndUpdate(cartId, {
+            await Cart.findByIdAndUpdate(setCartId, {
                 products: setProducts
             })
             res.json("Успешно удалено")
@@ -69,27 +95,29 @@ module.exports.cartController = {
     },
     productResetCart: async (req, res) => {
         try {
-            const { cartId } = req.params
+            const { userId } = req.params
+            const setUser = await User.findById(userId)
+            const setCart = await Cart.find({
+                userId: setUser._id
+            })
+            const setCartId = String(setCart[0]._id)
             
-            const setCart = await Cart.findById(cartId)
-            const setDeleteProduct = setCart.products
+            const setCart0 = await Cart.findById(setCartId)
+            const setDeleteProduct = setCart0.products
             
-            res.json(setCart.products)
             setDeleteProduct.forEach(async(el) => {
                 const setProduct0 = await Product.findById(el.productId)
                 await Product.findByIdAndUpdate(el.productId, {
                     left: setProduct0.left + el.amount
                 })
-                console.log(setProduct0)
 
             });
             
-            const nullArr = setCart.products.filter((el) => false)
-            await Cart.findByIdAndUpdate(cartId, {
+            const nullArr = setCart0.products.filter((el) => false)
+            await Cart.findByIdAndUpdate(setCartId, {
                 products: nullArr
             })
-
-            // res.json("Успешно добавлен в корзину")
+            res.json('Корзина успешно очищена')
 
         } catch (e) {
             res.json(e)
@@ -97,27 +125,32 @@ module.exports.cartController = {
     },
     productIncCart: async (req, res) => {
         try {
-            const { cartId } = req.params
+            const { userId } = req.params
+            const setUser = await User.findById(userId)
+            const setCart = await Cart.find({
+                userId: setUser._id
+            })
+            const setCartId = String(setCart[0]._id)
+
             const { product } = req.body
 
-            const setCart = await Cart.findById(cartId)
+            const setCart0 = await Cart.findById(setCartId)
             const setProduct = await Product.findById(product)
 
             if (setProduct.left < 1) {
                 return res.json("Нет на складе")
             }
 
-            const setProducts = setCart.products.map((el) => {
+            const setProducts = setCart0.products.map((el) => {
                 if (String(el.productId) === String(setProduct._id)) {
                     el.amount += 1
                 }
-                // res.json(el.productId)
                 return el
             })
 
 
 
-            await Cart.findByIdAndUpdate(cartId, {
+            await Cart.findByIdAndUpdate(setCartId, {
                 products: setProducts
             })
 
@@ -134,33 +167,39 @@ module.exports.cartController = {
     },
     productDecCart: async (req, res) => {
         try {
-            const { cartId } = req.params
+            const { userId } = req.params
+            const setUser = await User.findById(userId)
+            const setCart = await Cart.find({
+                userId: setUser._id
+            })
+            const setCartId = String(setCart[0]._id)
+
             const { product } = req.body
 
-            const setCart = await Cart.findById(cartId)
+            const setCart0 = await Cart.findById(setCartId)
             const setProduct = await Product.findById(product)
 
-            const setProducts = setCart.products.map((el) => {
+            if (setCart0.left < 1) {
+                return res.json("Нет на складе")
+            }
+
+            const setProducts = setCart0.products.map((el) => {
                 if (String(el.productId) === String(setProduct._id)) {
                     el.amount -= 1
                     return el
                 }
-                // res.json(el.productId)
                 return el
             })
 
 
 
-            await Cart.findByIdAndUpdate(cartId, {
+            await Cart.findByIdAndUpdate(setCartId, {
                 products: setProducts
             })
 
             await Product.findByIdAndUpdate(product, {
                 left: setProduct.left + 1
             })
-
-            // res.json(setProducts)
-            res.json(await Cart.findById(cartId))
 
             res.json('-')
 
@@ -170,5 +209,3 @@ module.exports.cartController = {
     },
 
 }
-
-// { "name": "Rabek", "surname": "Rabekov", "phone": "8999999999-000", "icon": "srcggggggg", "login": "aleksej223", "password": "0369" }
